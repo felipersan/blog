7175a26a573a2f1477c24d33c1529378983b3ffd
felipersan <feliperodrigues.san@gmail.com>
2023-02-23 22:17:35 -0300

    update files

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '535c514a47984f1fb76b31f25bbba384',
+  rootNotionPageId: 'd1c77de3ee8541a4b3811c1accc926d8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'CarHunt',
+  name: 'Agência Sanfilippo',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Carhunt',
+  author: 'Felipe Sanfilippo',
 
   // open graph metadata (optional)
-  description: 'Blog da CarHunt',
+  description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
   // github: 'transitive-bullshit',
-  // linkedin: 'felipe-sanfilippo-ab035b1a3/',
-  // whatsapp: 'https://wa.me/5511989893698',
+  linkedin: 'felipe-sanfilippo-ab035b1a3/',
+  whatsapp: 'https://wa.me/5511989893698',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
